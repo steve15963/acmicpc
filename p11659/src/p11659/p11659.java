d6054458f5bc83ae265db54140fe402026dbb3f1
steve15963 <steve15963@gmail.com>
2023-02-07 22:08:30 +0900

    p11659 708ms use StringBuilder and StringTokenizer

--- p11659/src/p11659/p11659.java
+++ p11659/src/p11659/p11659.java
@@ -3,27 +3,31 @@ package p11659;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class p11659 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		String lengthS[] = br.readLine().split(" "); 
-		String datas[] = br.readLine().split(" ");
-		int data[] = new int[datas.length+1];
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		int M = Integer.parseInt(lengthS[0]);
 		int N = Integer.parseInt(lengthS[1]);
+		int data[] = new int[M+1];
 		data[0] = 0;
-		for(int i=1; i <= M ;i++) {
-			data[i] = data[i-1] + Integer.parseInt(datas[i-1]);
+		int i = 1;
+		while(st.hasMoreTokens()) {
+			data[i] = data[i-1] + Integer.parseInt(st.nextToken());
+			i++;
 		}
 		
-		for(int i=0; i < N ;i++) {
+		for(int j=0; j < N ;j++) {
 			String point[] = br.readLine().split(" ");
 			int start = Integer.parseInt(point[0]) - 1;
 			int end = Integer.parseInt(point[1]);
-
-			System.out.println(data[end] - data[start]);
+			sb.append(data[end] - data[start]).append("\n");
 		}
+		System.out.println(sb.toString());
 	}
 }
