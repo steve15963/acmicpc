4e081b81bac8042fae0e0d5571dd442ef60c9d9f
송진현 <dkdkdkal123@naver.com>
2023-02-10 09:05:08 +0900

    p2292 clear 128ms

--- p2292/src/p2292/p2292.java
+++ p2292/src/p2292/p2292.java
@@ -1,10 +1,24 @@
 package p2292;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class p2292 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int input = Integer.parseInt(br.readLine());
+		int range = 2;
+		int round = 1;
+		if(input != 1) {
+			while(input >= range) {
+				range += 6 * round;
+				round++;
+			}
+		}
+		System.out.println(round);
 	}
 
 }
