b38944176f9682af84483ea6e43d7a41e4a36cb7
송진현 <dkdkdkal123@naver.com>
2023-02-13 17:56:01 +0900

    end day

--- p1158/src/p1158/p1158.java
+++ p1158/src/p1158/p1158.java
@@ -18,17 +18,22 @@ public class p1158 {
 		int K = Integer.parseInt(sData[1]);
 		
 		Queue<Integer> que = new LinkedList<Integer>();
+		// N개의 인원수 만큼 enqueue;
 		for(int i = 1; i <= N; i++) {
 			que.add(i);
 		}
-		
+		//1명이 남을때 까지.
 		while(que.size() > 1) {
+			//K - 1명을 돌리고.
 			for(int j = 0; j < K - 1;j++) {
 				que.add(que.poll());
 			}
+			// K번째 사람을 버퍼에 넣는다.
 			sb.append(que.poll()).append(", ");
 		}
+		//마지막 남은 사람을 버퍼에 넣고
 		sb.append(que.poll()).append(">");
+		//출력한다.
 		System.out.println(sb.toString());
 	}
 }
