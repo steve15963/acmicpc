1538b8e641262283ece0f617da35c71f2816e65b
steve15963 <steve15963@gmail.com>
2023-02-20 23:58:08 +0900

    sleep

---
+++ p2447/src/p2447/p2447.java
@@ -0,0 +1,23 @@
+package p2447;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p2447 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		recall(N);
+	}
+
+	private static void recall(int n) {
+		if(n == 3) {
+			
+		}
+		
+		
+	}
+
+}
