cc7ffe2c5845662e0a7cf2398ec100c7650e148b
송진현 <dkdkdkal123@naver.com>
2023-03-30 15:17:51 +0900

    p14502 clear 424ms

--- 17070/p17070/src/p17070/p17070.java
+++ 17070/p17070/src/p17070/p17070.java
@@ -3,24 +3,9 @@ package p17070;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class p17070 {
-	static class Point{
-		int x;
-		int y;
-		int d;
-		public Point(int x, int y, int d) {
-			super();
-			this.x = x;
-			this.y = y;
-			this.d = d;
-		}
-		
-	}
-
 	static int dy[] = {0,1,1};
 	static int dx[] = {1,1,0};
 	
@@ -45,66 +30,47 @@ public class p17070 {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
+		
+		//DFS 경로 추적
 		DFS(map,N,1,0,0);
 		System.out.println(count);
-
-		count = 0;
-		Queue<Point> queue = new ArrayDeque<>();
-		
-		queue.add(new Point(0,1,0));
-		while(!queue.isEmpty()) {
-			Point point = queue.poll();
-			int x = point.x;
-			int y = point.y;
-			if(x == N-1 && y == N-1) {
-				count++;
-				continue;
-			}
-			int d = point.d;
-			
-			for(int i = angle[d][0] ; i <= angle[d][1]; i++) {
-				int nextX = x + dx[i];
-				int nextY = y + dy[i];
-				if(
-						isCan(nextX,nextY,N) &&
-						isCollision(map,x,y,nextX,nextY,N,i)
-				) {
-					queue.add(new Point(nextX, nextY, i));
-				}
-			}
-		}
-		System.out.println(count);
-		
-		
-		
 	}
 	
 	private static void DFS(int[][] map, int n,int x, int y,int d) {
 		//기저 조건
 		if(x == n-1 && y == n-1) {
+			// 도착하면 카운트하고 종료
 			count++;
 			return;
 		}
 		//구현
+		//방향에 따라서 angle의 범위만큼만 스캔.
 		for(int i = angle[d][0] ; i <= angle[d][1]; i++) {
 			int nextX = x + dx[i];
 			int nextY = y + dy[i];
 			if(
+					// 가능한 좌표인지.
 					isCan(nextX,nextY,n) &&
+					// 가능한 좌표라면 충돌되는 벽이 없는지 검사
 					isCollision(map,x,y,nextX,nextY,n,i)
 			) {
+				//가능한 좌표이면서 충돌하는 벽이 없다면 이동.
 				DFS(map, n, nextX, nextY, i);
 			}
 		}
 	}
 	private static boolean isCollision(int map[][],int x, int y,int nX,int nY,int n, int d) {
+		// 대각선 이동이라면 3개의 좌표를 모두 검사.
 		if(d == 1) {
 			for(int i = 0 ; i < 3; i++) {
 				int nextX = x + dx[i];
 				int nextY = y + dy[i];
+				// 불가능한 좌표라면 다음 검사
 				if(!isCan(nextX,nextY,n)) continue;
+				// 벽이 있다면 false리턴
 				if(map[nextY][nextX] == 1) return false;
 			}
+			//모두 통과시 true 리턴
 			return true;
 		}
 		else {
@@ -114,6 +80,7 @@ public class p17070 {
 	}
 
 	private static boolean isCan(int x, int y, int n) {
+		// 좌표 유효성 검사.
 		return 0 <= x && x < n && 0 <= y && y < n;
 	}
 
---
+++ p14502/src/p14502/p14502.java
@@ -0,0 +1,91 @@
+package p14502;
+
+import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class p14502 {
+	static final int ny[] = {-1, 0, 1, 0};
+	static final int nx[] = { 0, 1, 0,-1};
+	
+	static int safeZone = 0;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int R = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		
+		int map[][] = new int[R][C];
+		
+		List<Point> virusList = new ArrayList<Point>();
+		
+		for(int i = 0 ; i < R; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0 ; j < C; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if(map[i][j] == 2)
+					virusList.add(new Point(j,i));
+			}
+		}
+		DrawWall(R,C,map,virusList,0,0);
+		System.out.println(safeZone);
+	}
+
+	private static void DrawWall(int R, int C, int[][] map,List<Point> virusList, int cnt, int start) {
+		if(cnt == 3) {
+			int copyMap[][] = new int[R][C];
+			for(int i = 0 ; i < R; i++) {
+				System.arraycopy(map[i], 0, copyMap[i], 0, C);
+			}
+			safeZone = Math.max(safeZone,ExpandVirus(R,C,copyMap,virusList));
+			return;
+		}
+		for(int i = start ; i < R * C; i++) {
+			if(map[ i / C][ i % C ] == 0) {
+				map[ i / C][ i % C ] = 1;
+				DrawWall(R, C, map,virusList, cnt + 1, i);
+				map[ i / C][ i % C ] = 0;
+ 			}
+		}
+	}
+	private static int ExpandVirus(int r, int c, int[][] map,List<Point> virusList) {
+		Queue<Point> ExpandVirusList = new ArrayDeque<>(virusList);
+		while(!ExpandVirusList.isEmpty()) {
+			Point target = ExpandVirusList.poll();
+			int x = target.x;
+			int y = target.y;
+			for(int i = 0 ; i < 4; i++) {
+				int nextX = x + nx[i];
+				int nextY = y + ny[i];
+				if(isCan(r,c,nextY,nextX) && map[nextY][nextX] == 0) {
+					map[nextY][nextX] = 3;
+					ExpandVirusList.add(new Point(nextX,nextY));
+				}
+			}
+		}
+		int count = 0;
+		for(int i = 0 ; i < r; i++) {
+			for(int j = 0 ; j < c;j++) {
+				if(map[i][j] == 0) 
+					count++;
+			}
+		}
+		return count;
+	}
+	private static boolean isCan(int r, int c, int y, int x) {
+		return 0 <= y && y < r && 0 <= x && x < c;
+	}
+}
